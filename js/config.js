495f144ca6e197dfc3dd59fa78153b3314a4015e
PKPSorong-lt2 <keuangan2024@polikpsorong.ac.id>
2026-01-06 21:44:11 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@
  */
 
 var CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbyHrPIlaCHdRB_6ImGx1oWzxkMQxwqSU7ZVHRAZUOE9Uwu39HjbzE4tXLBQCGFGqLv1/exec'
 };
